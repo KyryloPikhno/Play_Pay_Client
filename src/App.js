25d78c5b6c5c2220654cf6a1add9157da90a05c2
KyryloPikhno <Mr.Good@i.ua>
2023-02-28 11:21:12 +0200

    added redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import {Route, Routes, Navigate} from "react-router-dom";
-import {MainLayout} from "./layouts";
 
+import {MainLayout} from "./layouts";
 import {AccountPage, HomePage} from "./pages";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./shards-dashboard/styles/shards-dashboards.1.1.0.min.css";
