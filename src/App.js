b36afc91ee99710ed2a6251517f2a10ca2fb9b05
KyryloPikhno <Mr.Good@i.ua>
2023-02-28 11:44:37 +0200

    added redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import {Route, Routes, Navigate} from "react-router-dom";
+import {useEffect} from "react";
 
 import {MainLayout} from "./layouts";
 import {AccountPage, HomePage} from "./pages";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./shards-dashboard/styles/shards-dashboards.1.1.0.min.css";
+import icon from './images/favicon.ico';
+
 
 function App() {
+  useEffect(() => {
+    const favicon = document.getElementById('favicon');
+    favicon.setAttribute('href', icon);
+  }, []);
+
   return (
     <Routes>
       <Route path={'/'} element={<MainLayout/>}>
